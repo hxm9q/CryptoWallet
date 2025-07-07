ad60d6dfbba7ec31045817af7dd336af91c0e122
hxm9q <hxm9q@icloud.com>
2025-07-07 14:26:33 +0400

    project setup

---
+++ CryptoWallet/CryptoWallet/Modules/Auth/AuthViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class AuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue.withAlphaComponent(0.5)
+    }
+
+}
