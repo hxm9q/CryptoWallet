380eba6f7c6f84b00cbaa2e244f34b444230f3e7
hxm9q <hxm9q@icloud.com>
2025-07-08 13:29:34 +0400

    Coin Model

---
+++ CryptoWallet/CryptoWallet/Core/Models/Coin.swift
@@ -0,0 +1,11 @@
+import Foundation
+import UIKit
+
+struct Coin {
+    let name: String
+    let ticker: String
+    let image: UIImage?
+    let price: String
+    let change: String
+    let isPositiveChange: Bool
+}
