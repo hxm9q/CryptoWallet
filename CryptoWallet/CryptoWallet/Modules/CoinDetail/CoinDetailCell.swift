633f44c51295ecc34152de71d797a88c2741ec63
hxm9q <hxm9q@icloud.com>
2025-07-09 20:05:13 +0400

    UI on CoinDetail Screen

--- CryptoWallet/CryptoWallet/Modules/CoinDetail/CoinDetailCell.swift
+++ CryptoWallet/CryptoWallet/Modules/CoinDetail/CoinDetailCell.swift
@@ -1,8 +1,71 @@
-//
-//  CoinDetailCell.swift
-//  CryptoWallet
-//
-//  Created by  Антон Шадрин on 09.07.2025.
-//
+import UIKit
 
-import Foundation
+class CoinDetailCell: UITableViewCell {
+    
+    // MARK: - Identifier
+    static let identifier = "CoinDetailCell"
+    
+    // MARK: - UI Components
+    private let statisticLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Market Statistic"
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
+        return label
+    }()
+    
+    private let capitalizationLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Market capitalization"
+        label.font = .systemFont(ofSize: 14, weight: .medium)
+        label.textColor = .lightGray
+        return label
+    }()
+    
+    private let supplyLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Circulating Supply"
+        label.font = .systemFont(ofSize: 14, weight: .medium)
+        label.textColor = .lightGray
+        return label
+    }()
+    
+    private let capitalizationAmountLabel: UILabel = {
+        let label = UILabel()
+//        label.text = "\())" Добавить капитализацию в структуру Coin
+        return label
+    }()
+    
+    private let supplyAmountLabel: UILabel = {
+        let label = UILabel()
+//        label.text = "\())" Добавить supply в структуру Coin
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        backgroundColor = .clear
+        
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Methods
+    private func setupLayout() {
+        [statisticLabel, capitalizationLabel].forEach {
+            contentView.addSubview($0)
+        }
+        
+        statisticLabel.snp.makeConstraints { make in
+            
+        }
+    }
+    
+    func configure(with coin: Coin) {
+        capitalizationLabel.text = "" // Добавить из API
+    }
+}
--- CryptoWallet/CryptoWallet/Modules/CoinDetail/CoinDetailViewController.swift
+++ CryptoWallet/CryptoWallet/Modules/CoinDetail/CoinDetailViewController.swift
@@ -1,29 +1,184 @@
-//
-//  CoinDetailViewController.swift
-//  CryptoWallet
-//
-//  Created by  Антон Шадрин on 09.07.2025.
-//
-
 import UIKit
+import SnapKit
 
 class CoinDetailViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    static let testCoin = [
+        Coin(
+            name: "Etherium",
+            symbol: "ETH",
+            price: 32128.80,
+            change: 2.5
+        )
+    ]
+    
+    private let coin: Coin
+    private let timeFilterSections = ["24H", "1W", "1Y", "ALL", "Point"]
+    
+    // MARK: - UI Components
+    private let nameLabel = UILabel()
+    private let priceLabel = UILabel()
+    private let changeLabel = UILabel()
+    private let changeImageView = UIImageView()
+    private let segmentedControl = UISegmentedControl()
+    private let tableView = UITableView()
+    
+    private let navigationButton = UIButton()
+    
+    // MARK: - Init
+    init(coin: Coin) {
+        self.coin = coin
+        super.init(nibName: nil, bundle: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 243/255, green: 245/255, blue: 246/255, alpha: 1)
+        
+        setupUI()
+        setupLayout()
+        setupNavigationButton()
+    }
+}
 
+// MARK: - Setup UI
+private extension CoinDetailViewController {
+    
+    func setupUI() {
+        // MARK: Coin Name Label
+        nameLabel.text = "\(coin.name) (\(coin.symbol.uppercased()))"
+        nameLabel.font = .systemFont(ofSize: 14, weight: .semibold)
+        
+        // MARK: Coin Price Label
+        priceLabel.text = "$\(coin.price)"
+        priceLabel.font = .systemFont(ofSize: 28, weight: .semibold)
+        
+        // MARK: Change Label
+        changeLabel.text = "\(coin.change)"
+        changeLabel.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        changeLabel.textColor = .lightGray
+        
+        // MARK: Change Image Arrow
+        changeImageView.contentMode = .scaleAspectFit
+        let arrowImageName = coin.isPositiveChange ? "arrow up" : "arrow down"
+        changeImageView.image = UIImage(named: arrowImageName)
+        
+        // MARK: Segmented Control
+        segmentedControl.removeAllSegments()
+        for (index, title) in timeFilterSections.enumerated() {
+            segmentedControl.insertSegment(withTitle: title, at: index, animated: false)
+        }
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.addTarget(self, action: #selector(segmentedControlChanged), for: .valueChanged)
+        
+        // MARK: Table View
+        tableView.register(CoinDetailCell.self, forCellReuseIdentifier: CoinDetailCell.identifier)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.separatorStyle = .none
+        tableView.rowHeight = 30
+        tableView.layer.cornerRadius = 40
+        tableView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        tableView.clipsToBounds = true
+    }
+    
+    @objc private func segmentedControlChanged(_ sender: UISegmentedControl) {
+        let selectedValue = timeFilterSections[sender.selectedSegmentIndex]
+    }
+}
+
+// MARK: - Setup Layout
+private extension CoinDetailViewController {
+    
+    func setupLayout() {
+        [nameLabel, priceLabel, changeLabel, changeImageView, segmentedControl, tableView].forEach {
+            view.addSubview($0)
+        }
+        
+        nameLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.snp.top).offset(80)
+            make.centerX.equalToSuperview()
+            //            make.width.equalTo(106)
+            make.height.equalTo(21)
+        }
+        
+        priceLabel.snp.makeConstraints { make in
+            make.top.equalTo(nameLabel.snp.bottom).offset(20)
+            make.centerX.equalToSuperview()
+            //            make.width.equalTo(150)
+            make.height.equalTo(42)
+        }
+        
+        changeLabel.snp.makeConstraints { make in
+            make.top.equalTo(priceLabel.snp.bottom).offset(3)
+            make.leading.equalTo(view.snp.leading).offset(190)
+        }
+        
+        changeImageView.snp.makeConstraints { make in
+            make.top.equalTo(priceLabel.snp.bottom).offset(6)
+            make.leading.equalTo(view.snp.leading).offset(175)
+        }
+        
+        segmentedControl.snp.makeConstraints { make in
+            make.top.equalTo(changeLabel.snp.bottom).offset(24)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(325)
+            make.height.equalTo(56)
+        }
+        
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(470)
+            make.width.equalToSuperview()
+            make.height.equalTo(242)
+        }
+    }
+}
+
+// MARK: - Navigation Button Setup
+private extension CoinDetailViewController {
+    
+    func setupNavigationButton() {
+        navigationButton.setImage(UIImage(named: "back_button"), for: .normal)
+        navigationButton.imageView?.contentMode = .scaleToFill
+        navigationButton.layer.cornerRadius = 24
+        navigationButton.clipsToBounds = true
+        navigationButton.backgroundColor = .white.withAlphaComponent(0.8)
+        navigationButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+        
+        let barButtonItem = UIBarButtonItem(customView: navigationButton)
+        navigationItem.hidesBackButton = true
+        navigationItem.leftBarButtonItem = barButtonItem
+    }
+    
+    @objc func backButtonTapped() {
+        navigationController?.popViewController(animated: true)
+    }
+}
+
+// MARK: - TableViewDatasource & TableViewDelegate
+extension CoinDetailViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard
+            let cell = tableView.dequeueReusableCell(withIdentifier: CoinDetailCell.identifier, for: indexPath) as? CoinDetailCell
+        else {
+            return UITableViewCell()
+        }
+        cell.configure(with: coin)
+        return cell
+    }
+}
+
+// MARK: - Preview
+#Preview {
+    UINavigationController(rootViewController: CoinDetailViewController(coin: CoinDetailViewController.testCoin[0]))
 }
